9753a6dae9ccadb6f578e0b32e405ed7405b5cc8
yasirdar19 <y.dar28@ncf.edu>
2026-03-06 15:01:34 -0500

    Delete project5-main directory

--- project5-main/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer footer-center p-4 bg-base-100 text-base-content text-sm">
-      <p>Movie App © {new Date().getFullYear()}</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- project5-main/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="navbar bg-primary text-primary-content px-4">
-      <span className="text-xl font-bold">Movie App</span>
-    </header>
-  );
-};
-
-export default Header;
